b79968b64f309368a868337a0401784e49685684
Prabesh Subedi <subediprabesh1@gmail.com>
2018-04-10 12:13:27 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,4 +1,4 @@
-const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
+const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
 let call = 0;
 function init() {
   document.body.addEventListener('keydown', keyDownHandler);
