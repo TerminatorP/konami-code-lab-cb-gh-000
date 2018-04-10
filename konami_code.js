16c90956d439d03a235b2f83973b870abac8099d
Prabesh Subedi <subediprabesh1@gmail.com>
2018-04-10 11:31:55 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,10 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
-
+let call = 0;
 function init() {
-  // Write your JavaScript code inside the init() function
-
+  document.body.addEventListener('keydown', keyDownHandler);
+  function keyDownHandler(e) {
+    let keyValue = parseInt(e.which || e.detail);
+  }
 }
+
+init();
