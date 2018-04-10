da4ed0214d5648d1eb74a3ad20efb7601c6fe97b
Prabesh Subedi <subediprabesh1@gmail.com>
2018-04-10 12:12:28 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,18 +1,16 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 let call = 0;
 function init() {
-  // document.body.addEventListener('keydown', keyDownHandler);
-  // function keyDownHandler(e) {
-  //   const key = parseInt(e.detail || e.which);
-  //   if (key === code[call]) {
-  //     call++;
-  //     if (call === code.length) {
-  //       window.alert("Hurray!");
-  //       call = 0;
-  //     }
-  //   }
-  //   else call = 0;
-  // }
+  document.body.addEventListener('keydown', keyDownHandler);
+  function keyDownHandler(e) {
+    const key = parseInt(e.detail || e.which);
+    if (key === code[call]) {
+      call++;
+      if (call === code.length) {
+        window.alert("Hurray!");
+        call = 0;
+      }
+    }
+    else call = 0;
+  }
 }
-
-// init();
